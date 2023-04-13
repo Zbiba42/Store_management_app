6a4f6f84ed13d1ca76eeb993e8c74813a2cdcc61
Zbiba <abdrrahmanabdo46@gmail.com>
2023-04-13 17:15:30 +0000

    finished the nav and the search functions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 import './Nav.css'
+
 export const Nav = () => {
+  const navigate = useNavigate()
+  const searchInput = useRef()
+  const SearchCollection = useRef()
+
+  const Search = async () => {
+    const searchTerm = searchInput.current.value
+    const Collection = SearchCollection.current.value
+    navigate('/Résultats', {
+      state: { searchTerm: searchTerm, Collection: Collection },
+    })
+  }
+
   return (
     <>
-      <div className="nav"></div>
+      <div className="navBar">
+        <div className="menu">
+          <select name="" id="" ref={SearchCollection}>
+            <option value="clients">Client</option>
+            <option value="products">Produit</option>
+          </select>
+        </div>
+
+        <input
+          className="search"
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+          ref={searchInput}
+        />
+        <i className="fa-solid fa-magnifying-glass" onClick={Search}></i>
+      </div>
     </>
   )
 }

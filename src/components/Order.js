f4cd7c7611a47ddf503c5ce58358437c7b4fc473
Zbiba <abdrrahmanabdo46@gmail.com>
2023-04-18 14:34:06 +0000

    small changes

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom'
 export const Order = ({ order }) => {
   const [Client, setClient] = useState()
   const navigate = useNavigate()
-  console.log(order)
   const getClient = async () => {
     const client = await axios.get(
       `http://127.0.0.1:8090/api/collections/clients/records/${order.client}`
@@ -24,7 +23,7 @@ export const Order = ({ order }) => {
         <tr onClick={GotoPage}>
           <td scope="col">{order.date}</td>
           <td scope="col">{Client.nom + ' ' + Client.prenom}</td>
-          <td scope="col">{order.produits.split('/').length - 1}</td>
+          <td scope="col">{JSON.parse(order.produits).length}</td>
           <td scope="col">{order.total + ' MAD'}</td>
         </tr>
       </>

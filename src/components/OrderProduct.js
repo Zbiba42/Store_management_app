49c4c00dfa2dc0ceb56c6698ab529bf8f6df9799
Zbiba <abdrrahmanabdo46@gmail.com>
2023-04-18 14:34:42 +0000

    created the component for the order page

---
+++ src/components/OrderProduct.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const OrderProduct = ({ product }) => {
+  const path = `http://127.0.0.1:8090/api/files/iwkp5worhbpetih/${product.id}/${product.image}`
+  return (
+    <>
+      <tr>
+        <td>
+          <img src={path} class="img-thumbnail" style={{ width: '100px' }} />
+        </td>
+        <td style={{ verticalAlign: 'middle' }}>{product.nom}</td>
+        <td style={{ verticalAlign: 'middle' }}>{product.marque}</td>
+        <td style={{ verticalAlign: 'middle' }}>{product.description}</td>
+        <td style={{ verticalAlign: 'middle' }}>{product.prix}</td>
+        <td style={{ verticalAlign: 'middle' }}>{product.quantity}</td>
+      </tr>
+    </>
+  )
+}
